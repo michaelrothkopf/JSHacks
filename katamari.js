ebf725fbf930f8f0471e0c9f3770dfb87f2aed19
chat-t <michaelhrothkopf@gmail.com>
2020-03-05 14:30:55 -0500

    Create katamari.js

---
+++ katamari.js
@@ -0,0 +1,2 @@
+// This hack is not made by us. Check out the hack's website at kathack.com!
+javascript:var i,s,ss=['http://kathack.com/js/kh.js','http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
