2b9322c91c2547f41e08d369849a4938bd20812b
mrbros35 <michaelhrothkopf@gmail.com>
2021-12-07 11:30:26 -0500

    Delete change_title_icon_googledocs.js

--- change_title_icon_googledocs.js
+++
@@ -1,8 +0,0 @@
-// To use in the URL bar, type javascript: and paste the code.
-var link = document.querySelector("link[rel*='icon']") || document.createElement('link'); 
-link.type = 'image/x-icon'; 
-link.rel = 'shortcut icon'; 
-link.href = 'https://ssl.gstatic.com/docs/documents/images/kix-favicon7.ico'; 
-document.getElementsByTagName('head')[0].appendChild(link); 
-document.title = "Google Docs"; 
-void 0
