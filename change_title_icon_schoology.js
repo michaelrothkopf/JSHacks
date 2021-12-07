c6fb276c30859c7a1c5956a95164e9fe944415cb
mrbros35 <michaelhrothkopf@gmail.com>
2021-12-07 11:30:18 -0500

    Delete change_title_icon_schoology.js

--- change_title_icon_schoology.js
+++
@@ -1,8 +0,0 @@
-// To use this in the URL bar, type javascript: and then paste the code.
-var link = document.querySelector("link[rel*='icon']") || document.createElement('link'); 
-link.type = 'image/x-icon'; 
-link.rel = 'shortcut icon'; 
-link.href = 'https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico'; 
-document.getElementsByTagName('head')[0].appendChild(link); 
-document.title = "Home | Schoology"; 
-void 0
